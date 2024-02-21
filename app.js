e834a6d5e600ce91a0cae35105a70eeba4c12e17
CesarBerta <cesar_fb@hotmail.com>
2024-02-20 21:46:11 -0300

    Add files login.html and register.html

--- app.js
+++ app.js
@@ -18,7 +18,18 @@ app.get("/", (req,res)=>{
     res.sendFile(path.resolve(__dirname, "./views/home.html"))
 });
 
-/* 
-app.get("/html", (req,res)=>{
-    res.send("Probando funcionamiento hipervinculo");
-}); */
+/* ruta localhost:3033/register */
+app.get("/register", (req,res)=>{
+    res.sendFile(path.resolve(__dirname, "./views/register.html"))
+});
+app.post("/registro", (req,res)=>{
+    res.send("Registrado de manera exitosa.")
+});
+
+
+/* ruta localhost:3033/login */
+app.get("/login", (req,res)=>{
+    res.sendFile(path.resolve(__dirname, "./views/login.html"))
+});
+
+
