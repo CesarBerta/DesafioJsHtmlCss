e58a82157b4f1c23b12ca08e14f8cce0c39ce5c4
CesarBerta <cesar_fb@hotmail.com>
2024-02-25 13:19:54 -0300

    Modifications all files

--- app.js
+++ app.js
@@ -3,13 +3,11 @@ const app = express();
 
 const path = require ("path");
 
-// ruta absoluta carpeta public
 const publicPath = path.resolve(__dirname , "./public");
 
 app.use (express.static (publicPath));
 
-/*inicializo servidor 3033 
-ruta localhost:3033*/
+/*inicializo servidor 3033 ruta localhost:3033*/
 app.listen (3033, () => 
     console.log("Servidor corriendo en Puerto 3033")
 );
@@ -18,7 +16,6 @@ app.get("/", (req,res)=>{
     res.sendFile(path.resolve(__dirname, "./views/home.html"))
 });
 
-/* ruta localhost:3033/register */
 app.get("/register", (req,res)=>{
     res.sendFile(path.resolve(__dirname, "./views/register.html"))
 });
@@ -26,10 +23,6 @@ app.post("/registro", (req,res)=>{
     res.send("Registrado de manera exitosa.")
 });
 
-
-/* ruta localhost:3033/login */
 app.get("/login", (req,res)=>{
     res.sendFile(path.resolve(__dirname, "./views/login.html"))
 });
-
-
