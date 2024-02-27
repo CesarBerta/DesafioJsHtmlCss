859d96c14c44f17a91bf1e9682b1ca0e4efeb5e6
CesarBerta <cesar_fb@hotmail.com>
2024-02-27 18:19:53 -0300

    Modified app.js

--- app.js
+++ app.js
@@ -2,14 +2,14 @@ const express = require("express");
 const app = express();
 
 const path = require ("path");
-
 const publicPath = path.resolve(__dirname , "./public");
 
 app.use (express.static (publicPath));
 
-/*inicializo servidor 3033 ruta localhost:3033*/
-app.listen (3033, () => 
-    console.log("Servidor corriendo en Puerto 3033")
+/* inicializo PORT en render o servidor 3033 ruta localhost:3033 */
+const port = process.env.PORT || 3033;
+app.listen (port, () => 
+    console.log(`Servidor corriendo en Puerto ${port}`)
 );
 
 app.get("/", (req,res)=>{
